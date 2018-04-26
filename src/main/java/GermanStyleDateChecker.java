e869a509af1c09419b1c51a7baf32480037c6db7
Unknown <brandmairstefan@gmail.com>
2018-04-26 09:39:56 +0200

    Well, it works.

--- src/main/java/GermanStyleDateChecker.java
+++ src/main/java/GermanStyleDateChecker.java
@@ -2,7 +2,11 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class GermanStyleDateChecker {
+
+    private static final String GERMAN_DATE_REGEX = "^([1-9]|[0-2][0-9]|3[01])[ .-](0?[1-9]|1[012])[ .-]((19|20)?[0-9]{2})$";
+    
     public static Matcher getMatcher(String dateString) {
-        return null;
+        Pattern p = Pattern.compile(GERMAN_DATE_REGEX);
+        return p.matcher(dateString);
     }
 }
